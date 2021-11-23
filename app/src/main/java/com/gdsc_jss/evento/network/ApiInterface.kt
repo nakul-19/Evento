8bcd18ae4b5af98b46c5dd2e6921b2850c98ed26
Karan Verma <karanverma9335@gmail.com>
2021-11-23 16:48:29 +0530

    Added SignIn and SignUp Functionality.

--- app/src/main/java/com/gdsc_jss/evento/network/ApiInterface.kt
+++ app/src/main/java/com/gdsc_jss/evento/network/ApiInterface.kt
@@ -1,4 +1,24 @@
 package com.gdsc_jss.evento.network
 
+import com.gdsc_jss.evento.network.models.SignInBody
+import com.gdsc_jss.evento.network.models.SignInResponse
+import com.gdsc_jss.evento.network.models.SignUpResponse
+import com.gdsc_jss.evento.network.models.SignupBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiInterface {
+
+    companion object {
+        const val LOGIN = "api/user/login"
+        const val REGISTER = "api/user"
+    }
+
+    @POST(LOGIN)
+    suspend fun login(@Body signBody: SignInBody): Response<SignInResponse>
+
+    @POST(REGISTER)
+    suspend fun signUp(@Body signupBody: SignupBody): Response<SignUpResponse>
+
 }
--- app/src/main/java/com/gdsc_jss/evento/network/repositories/LoginRepository.kt
+++ app/src/main/java/com/gdsc_jss/evento/network/repositories/LoginRepository.kt
@@ -2,9 +2,21 @@ package com.gdsc_jss.evento.network.repositories
 
 import com.gdsc_jss.evento.network.ApiInterface
 import com.gdsc_jss.evento.network.SafeApiRequest
+import com.gdsc_jss.evento.network.models.SignInBody
+import com.gdsc_jss.evento.network.models.SignInResponse
+import com.gdsc_jss.evento.network.models.SignUpResponse
+import com.gdsc_jss.evento.network.models.SignupBody
+import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
+
 
 class LoginRepository @Inject constructor(private val networkApi: ApiInterface) :
     SafeApiRequest() {
+
+    suspend fun signIn(signBody: SignInBody): SignInResponse =
+        apiRequest { networkApi.login(signBody) }
+
+    suspend fun signUp(signupBody: SignupBody): SignUpResponse =
+        apiRequest { networkApi.signUp(signupBody) }
 
 }
